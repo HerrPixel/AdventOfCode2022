8c8d574f832e1d17aa9e3b1fefd62ed840441c6d
Jonas Seiler <Me@jonasseiler.de>
2022-12-01 14:41:32 +0100

    Added Explanation

--- Day1/Calories.jl
+++ Day1/Calories.jl
@@ -2,6 +2,11 @@ using DataStructures
 
 # Answer to Puzzle 1 is CaloriesOfTopElves(1)
 # Answer to Puzzle 2 is CaloriesOfTopElves(3)
+
+# We iterate over the lines of input and sum up consecutive number lines.
+# When reading an empty line, we instead push the value to a minHeap and pop the lowest value,
+# therefore only keeping the top #NrOfTopElves values
+# Lastly when we finished reading the input, we output the sum of all remaining values in the heap.
 function CaloriesOfTopElves(NrOfTopElves::Integer=1)
     CalorieHeap = BinaryMinHeap{Int}(zeros(NrOfTopElves))
     currCalories = 0
