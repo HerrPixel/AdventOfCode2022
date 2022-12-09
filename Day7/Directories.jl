84ce8df1b039e9dde75b10fbcc411a61a9ccd692
Jonas Seiler <Me@jonasseiler.de>
2022-12-09 22:09:35 +0100

    Added Explanation for Day7

--- Day7/Directories.jl
+++ Day7/Directories.jl
@@ -1,48 +1,50 @@
 using DataStructures
 
+# answer to puzzle 1 is getSizesBelowThreshold(100000)
+# answer to puzzle 2 is makeEnoughSpace(30000000,70000000)
+
+# We get the sizes of all directories
+# and then just filter out those that are too big.
+# Lastly we sum up those valid sizes
 function getSizesBelowThreshold(lowerThan::Integer)
     DirSizes = getDirectorySizes()
     println(sum(filter(x -> x ≤ lowerThan, DirSizes)))
 end
 
+# We again get the sizes of all directories
+# and then filter out those directories that are too small
+# lastly we choose that valid directory that is smallest
+# among all directories big enough.
 function makeEnoughSpace(spaceNeeded::Integer, maxSystemSpace::Integer)
     DirSizes = getDirectorySizes()
-    #println(DirSizes[1])
-    #println(DirSizes)
     currSpaceLeft = maxSystemSpace - last(DirSizes)
     currSpaceNeeded = spaceNeeded - currSpaceLeft
     DirSizes = filter(x -> x ≥ currSpaceNeeded, DirSizes)
     println(reduce(min,DirSizes))
 end
 
-#=
-    cap = resolveStack2(Nodes, SizesOfNodes)
-    println("We have a size of ", cap)
-    need = 30000000 - (70000000 - cap)
-    best = 70000000
-    for i in SizesOfNodes
-        if i ≥ need && i < best
-            best = i
-        end
-    end
-    println(best)
-
-
-
-=#
-
+# Since the input parses the directory structure via 
+# depth first search, we can keep the directories in a Stack
+# and push when we go down a directory and pop when we go up.
+# Since each directory is only mentioned once, we can savely store
+# the final size into a vector of directory sizes
 function getDirectorySizes()
     SizesOfNodes = Vector{Integer}()
     Nodes = Stack{Integer}()
+
     for line in eachline("./Day7/input.txt")
         parseLine(line, Nodes, SizesOfNodes)
     end
+
+    # since the input does not go up to the initial / directory
+    # we still need to resolve our current stack, since we update
+    # parent directory sizes when poping its children
     resolveStack(Nodes, SizesOfNodes)
     return SizesOfNodes
-    #println(sum(filter( x -> x ≤ LowerThan, SizesOfNodes)))
 end
 
 function parseLine(line::String, Nodes::Stack{Integer}, SizesOfNodes::Vector{Integer})
+    # A command and a listing can be distuingished by the '$' symbol
     if line[1] == '$'
         parseCommand(line,Nodes,SizesOfNodes)
     else
@@ -52,6 +54,8 @@ function parseLine(line::String, Nodes::Stack{Integer}, SizesOfNodes::Vector{Int
 end
 
 function parseDirectoryEntry(entry::String, Nodes::Stack{Integer})
+    # We don't care about directory entries since we solve those 
+    # when we actually go down that directory
     if entry[1] != 'd'
         addFileSize(entry,Nodes)
     end
@@ -59,12 +63,16 @@ function parseDirectoryEntry(entry::String, Nodes::Stack{Integer})
 end
 
 function addFileSize(entry::String, Nodes::Stack{Integer})
+    # The file size is always the starting argument,
+    # the file name does not matter to us, so we don't store that
     output = split(entry, " ")
     push!(Nodes, pop!(Nodes) + parse(Int, output[1]))
     return
 end
 
 function parseCommand(command::String, Nodes::Stack{Integer}, SizesOfNodes::Vector{Integer})
+    # We don't care about the line with the "ls" command so we ignore those
+    # after the "ls" command, we get the listing and we solve those in the relevant lines
     if command[3] == 'c'
         parseDirectoryChange(command, Nodes, SizesOfNodes)
     end
@@ -72,6 +80,8 @@ function parseCommand(command::String, Nodes::Stack{Integer}, SizesOfNodes::Vect
 end
 
 function parseDirectoryChange(command::String, Nodes::Stack{Integer}, SizesOfNodes::Vector{Integer})
+    # an argument beginning with a dot signifies that we go up a directory,
+    # otherwise we head further down
     if command[6] == '.'
         GoUpDirectory(Nodes, SizesOfNodes)
     else
@@ -80,6 +90,8 @@ function parseDirectoryChange(command::String, Nodes::Stack{Integer}, SizesOfNod
     return
 end
 
+# We add the size of the current directory to its parent directory
+# and then pop our just finished directory from the stack
 function GoUpDirectory(Nodes::Stack{Integer}, SizesOfNodes::Vector{Integer}) 
     DirectorySize = pop!(Nodes)
     push!(SizesOfNodes, DirectorySize)
@@ -87,71 +99,21 @@ function GoUpDirectory(Nodes::Stack{Integer}, SizesOfNodes::Vector{Integer})
     return
 end
 
+# we add another node for that just traversed directory
 function GoDownDirectory(Nodes::Stack{Integer})
     push!(Nodes,0)
     return
 end
 
+# finally, we add the sizes of the recursive directories to their parents 
 function resolveStack(Nodes::Stack{Integer}, SizesOfNodes::Vector{Integer})
     while !isempty(Nodes)
         dirSize = pop!(Nodes)
         push!(SizesOfNodes, dirSize)
+
+        # we cant add the size of / to anything
         if !isempty(Nodes)
             push!(Nodes, pop!(Nodes) + dirSize)
         end
     end 
 end
-
-#--------------------------------------
-
-#=
-function getBest()
-    SizesOfNodes = Vector{Int64}()
-    Nodes = Stack{fNode}()
-    for line in eachline("./Day7/input.txt")
-        parseCommand2(line, Nodes, SizesOfNodes)
-    end
-    cap = resolveStack2(Nodes, SizesOfNodes)
-    println("We have a size of ", cap)
-    need = 30000000 - (70000000 - cap)
-    best = 70000000
-    for i in SizesOfNodes
-        if i ≥ need && i < best
-            best = i
-        end
-    end
-    println(best)
-end
-
-function parseCommand2(command::String, Nodes::Stack{fNode}, SizesOfNodes::Vector{Int64})
-    if command[1] == '$'
-        if command[3] == 'c'
-            if command[6] == '.'
-                dirSize = getSize(first(Nodes))
-                push!(SizesOfNodes, dirSize)
-                pop!(Nodes)
-                addSize!(first(Nodes), dirSize)
-            else
-                push!(Nodes, fNode(0))
-            end
-        end
-    elseif command[1] != 'd'
-        output = split(command, " ")
-        addSize!(first(Nodes), parse(Int64, output[1]))
-    end
-end
-
-function resolveStack2(Nodes::Stack{fNode}, SizesOfNodes::Vector{Int64})
-    while !isempty(Nodes)
-        dirSize = getSize(first(Nodes))
-        push!(SizesOfNodes, dirSize)
-        pop!(Nodes)
-        if !isempty(Nodes)
-            addSize!(first(Nodes), dirSize)
-        else
-            return dirSize
-        end
-    end
-end
-
-=#
