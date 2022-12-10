61896fb9c4f1f36f3ee83c922f9cdbc8065fcf28
Jonas Seiler <Me@jonasseiler.de>
2022-12-10 20:02:12 +0100

    cleaned up, optimized and added Explanation for Day10

--- Day10/Registers.jl
+++ Day10/Registers.jl
@@ -1,61 +1,95 @@
+# Answer to Puzzle 1 is getSumOfSignalStrength()
+# Answer to Puzzle 2 is getCRTOutput() Attention: This is a picture!
 
+# Both puzzles use the same logic for reading lines, 
+# we just pass a puzzle specific function to the input parser
+# that needs to be checked every cycle.
 function getSumOfSignalStrength()
-    currRegister = 1
-    currCycle = 1
     sumOfImportantSignals = 0
 
-    for line in eachline("./Day10/input.txt")
-        if currCycle ∈ Set([20,60,100,140,180,220])
-            sumOfImportantSignals += (currCycle * currRegister)
-        end
-        if line[1] == 'a'
-            value = parse(Int,split(line," ")[2])
-            if currCycle + 1 ∈ Set([20,60,100,140,180,220])
-                sumOfImportantSignals += ( (currCycle + 1) * currRegister)
-            end
-            currRegister += value
-            currCycle += 2
-        else
-            currCycle += 1
-        end
-    end
-
+    sumOfImportantSignals = parseInput(cycleCheck, sumOfImportantSignals)
     println(sumOfImportantSignals)
 end
 
+# Both puzzles use the same logic for reading lines, 
+# we just pass a puzzle specific function to the input parser
+# that needs to be checked every cycle.
 function getCRTOutput()
-    screen = falses(40,6)
+    screen = falses(40, 6)
 
-    currRegister = 1
-    currScreenPosition = 0
-
-    for line in eachline("./Day10/input.txt")
-        screen = checkToDrawPixel(currRegister,currScreenPosition,screen)
-        if line[1] == 'a'
-            value = parse(Int,split(line," ")[2])
-            screen = checkToDrawPixel(currRegister, currScreenPosition + 1, screen)
-            currRegister += value
-            currScreenPosition += 1
-        end
-        currScreenPosition += 1
-    end
+    screen = parseInput(pixelCheck, screen)
     drawPicture(screen)
 end
 
-function checkToDrawPixel(currRegister::Integer, currScreenPosition::Integer,screen::BitMatrix)
-    horizontalPosition = mod(currScreenPosition,40)
-    if horizontalPosition ∈ Set([currRegister-1 , currRegister, currRegister+1])
-        index = divrem(currScreenPosition, 40)
-        screen[index[2] + 1, index[1] + 1] = true
+# This just parses the input and updates the register and cycles accordingly.
+# You can parse a function and an additional argument to this 
+# that gets updated every cycle
+function parseInput(applyEachCycle::Function, additionalArgument)
+    currRegister = 1
+    currCycle = 1
+
+    # since the cycles are not counted by the line number but seperately,
+    # we can just simulate the next step when we read an add operation
+    # and then update the cycle by 2 after that
+    for line in eachline("./Day10/input.txt")
+
+        # we apply this function on every cycle
+        additionalArgument = applyEachCycle(currCycle, currRegister, additionalArgument)
+
+        # if we read an add operation, we simulate the cycle after that 
+        # by passing the cycle number + 1 to the function and then 
+        # update the cycle number by 2 and update the register after that.
+        if line[1] == 'a'
+            value = parse(Int, split(line, " ")[2])
+
+            # simulation of the next cycle by passing the cycle number from the future
+            additionalArgument = applyEachCycle(currCycle + 1, currRegister, additionalArgument)
+
+            # now we can skip ahead one step
+            currRegister += value
+            currCycle += 1
+        end
+        currCycle += 1
+    end
+
+    return additionalArgument
+end
+
+# This just checks if the cycle is at one of the important points
+# and updates the signal sum if it is necessary
+function cycleCheck(cycle::Integer, registervalue::Integer, sumOfImportantSignals)
+    if cycle ∈ Set([20, 60, 100, 140, 180, 220])
+        sumOfImportantSignals += (cycle * registervalue)
+    end
+    return sumOfImportantSignals
+end
+
+# this checks if the register and the horizontal position of the CRT match
+# and draws a pixel accordingly
+function pixelCheck(cycle::Integer, registervalue::Integer, screen::BitMatrix)
+    cycle += -1
+
+    # we can get the horizontal position of the CRT with this operation
+    horizontalPosition = mod(cycle, 40)
+
+    # the sprite extends one pixel in each direction so we check all occupied spaces
+    if horizontalPosition ∈ Set([registervalue - 1, registervalue, registervalue + 1])
+
+        # should we hit it, we get the position of the CRT by division and remainder with 40
+        index = divrem(cycle, 40)
+        
+        # Careful, Julia starts its indices with 1!
+        screen[index[2]+1, index[1]+1] = true
     end
     return screen
 end
 
+# Just a drawing function for the screen
 function drawPicture(screen::BitMatrix)
-    for i in 1:size(screen,2)
-        for j in 1:size(screen,1)
-            if screen[j,i] == true
-                print("#")
+    for i in axes(screen, 2)
+        for j in axes(screen, 1)
+            if screen[j, i] == true
+                print("█")
             else
                 print(" ")
             end
