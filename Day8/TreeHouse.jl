e8303c95cb110838274e75d4be2dd20e6de03029
Jonas Seiler <Me@jonasseiler.de>
2022-12-10 17:20:13 +0100

    Better and cleaner version of Day8

--- Day8/TreeHouse.jl
+++ Day8/TreeHouse.jl
@@ -1,139 +1,107 @@
+# Answer to puzzle 1 is visibleTrees()
+# Answer to puzzle 2 is scenicScore()
+
 function visibleTrees()
-    grid = Vector{Vector{Integer}}()
-    i = 1
-    NoOfVisibleTrees = 0
-
-    for line in eachline("./Day8/test.txt") 
-        push!(grid,Vector{Int}())
-        j = 1
-        for c in line
-            push!(grid[i],parse(Int,c))
-            j += 1
-        end
-        i += 1
-    end
-
-    NotSeenBefore = trues(length(grid),length(grid[1]))
-
-    # top down
-    referenceElement = -1
-    for i in 1:length(grid)
-        for j in 1:length(grid[i])
-            if grid[i][j] > referenceElement
-                referenceElement = grid[i][j]
-                if NotSeenBefore[i,j]
-                    NoOfVisibleTrees += 1
-                    NotSeenBefore[i,j] = false   
-                end
-            end
-        end
-        referenceElement = -1
-    end
-
     
-    # left to right
-    for j in 1:length(grid[1]) 
-        for i in 1:length(grid)
-            if grid[i][j] > referenceElement
-                referenceElement = grid[i][j]
-                if NotSeenBefore[i,j]
-                    NoOfVisibleTrees += 1
-                    NotSeenBefore[i,j] = false   
+    function iterateHorizontalFirst(xRange::OrdinalRange{<:Integer,<:Integer}, yRange::OrdinalRange{<:Integer,<:Integer})
+        referenceElement = -1
+        for i in xRange
+            for j in yRange
+                if grid[i][j] > referenceElement
+                    referenceElement = grid[i][j]
+                    if NotSeenBefore[i,j]
+                        NoOfVisibleTrees += 1
+                        NotSeenBefore[i,j] = false
+                    end
                 end
             end
-        end
-        referenceElement = -1
+            referenceElement = -1
+        end 
     end
 
-    # down to up
-    for i in 1:length(grid)
-        for j in length(grid[i]):-1:1
-            if grid[i][j] > referenceElement
-                referenceElement = grid[i][j]
-                if NotSeenBefore[i,j]
-                    NoOfVisibleTrees += 1
-                    NotSeenBefore[i,j] = false   
+    function iterateVerticalFirst(xRange::OrdinalRange{<: Integer,<: Integer}, yRange::OrdinalRange{<: Integer,<:Integer})
+        referenceElement = -1
+        for j in yRange
+            for i in xRange
+                if grid[i][j] > referenceElement
+                    referenceElement = grid[i][j]
+                    if NotSeenBefore[i,j]
+                        NoOfVisibleTrees += 1
+                        NotSeenBefore[i,j] = false
+                    end
                 end
             end
-        end
-        referenceElement = -1
+            referenceElement = -1
+        end 
     end
+    
+    grid = initializeGrid()
+    NoOfVisibleTrees = 0
+    width = length(grid)
+    height = length(grid[1])
+    NotSeenBefore = trues(width,height)
+    
 
-    # right to left
-    for j in 1:length(grid[1]) 
-        for i in length(grid):-1:1
-            if grid[i][j] > referenceElement
-                referenceElement = grid[i][j]
-                if NotSeenBefore[i,j]
-                    NoOfVisibleTrees += 1
-                    NotSeenBefore[i,j] = false   
-                end
-            end
-        end
-        referenceElement = -1
-    end
+    iterateHorizontalFirst(1:width, 1:height) 
+    iterateHorizontalFirst(1:width, height:-1:1)
+    iterateVerticalFirst(1:width,1:height)
+    iterateVerticalFirst(width:-1:1,1:height)
 
     println(NoOfVisibleTrees)
 end
 
 function scenicScore()
-    grid = Vector{Vector{Integer}}()
-    i = 1
 
-    for line in eachline("./Day8/input.txt") 
-        push!(grid,Vector{Int}())
-        j = 1
-        for c in line
-            push!(grid[i],parse(Int,c))
-            j += 1
+    function iterateHorizontal(x::Integer, xRange::OrdinalRange{<:Integer,<:Integer}, y::Integer)
+        currHeight = grid[x][y]
+        for l in xRange
+            if grid[l][y] ≥ currHeight || l == last(xRange)
+                return abs(x-l)
+            end
         end
-        i += 1
     end
 
+    function iterateVertical(x::Integer, y::Integer, yRange::OrdinalRange{<:Integer,<:Integer})
+        currHeight = grid[x][y]
+        for l in yRange
+            if grid[x][l] ≥ currHeight || l == last(yRange)
+                return abs(y-l)
+            end
+        end
+    end
+
+    grid = initializeGrid()
+    width = length(grid)
+    height = length(grid[1])
     maxScore = 0
     currScore = 1
-    for i in 2:length(grid) -1
-        for j in 2:length(grid[i]) -1
-            currHeight = grid[i][j]
 
-            for l in i-1:-1:1
-                if grid[l][j] ≥ currHeight || l == 1
-                    currScore *= (i - l)
-                    break
-                    #println("curr scenic score for [", i, j, "is " , currScore)
-                end
-            end
-            
+    for i in 2:width-1
+        for j in 2:height-1
 
-            for l in i+1:length(grid)
-                if grid[l][j] ≥ currHeight || l == length(grid)
-                    currScore *= (l - i)
-                    break
-                end
-            end
+            currScore *= iterateHorizontal(i, i-1:-1:1, j)
+            currScore *= iterateHorizontal(i, i+1:width, j,)
+            currScore *= iterateVertical(i, j, j-1:-1:1)
+            currScore *= iterateVertical(i, j, j+1:height)
 
-            for l in j-1:-1:1
-                if grid[i][l] ≥ currHeight || l == 1
-                    currScore *= (j - l)
-                    break
-                end
-            end
-
-            for l in j+1:length(grid[i])
-                if grid[i][l] ≥ currHeight || l == length(grid[i])
-                    currScore *= (l - j)
-                    break
-                end
-            end
-
-            #println("curr scenic score for [", i, j, "is " , currScore)
-
-            if currScore > maxScore
-                maxScore = currScore
-            end
-
+            maxScore = max(currScore,maxScore)
             currScore = 1
         end
     end
     println(maxScore)
+end
+
+function initializeGrid()
+    grid = Vector{Vector{Integer}}()
+
+    i = 1
+    for line in eachline("./Day8/input.txt") 
+        push!(grid,Vector{Int}())
+        for c in eachindex(line)
+            push!(grid[i],parse(Int,line[c]))
+        end
+        i += 1
+    end
+
+    return grid
 end
