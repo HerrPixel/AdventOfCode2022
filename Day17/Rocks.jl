5306ca8fe10e1b8fcf21a275b48cea0f7f430a13
Jonas Seiler <Me@jonasseiler.de>
2022-12-30 22:31:28 +0100

    Cleaned up, optimized and added Explanation for Day17

--- Day17/Rocks.jl
+++ Day17/Rocks.jl
@@ -1,271 +1,210 @@
+# Solution to puzzle 1 is simulateRocks(2022)
+# Solution to puzzle 2 is simulateRocks(1000000000000)
+
+# helper struct for the five types of rocks, 
+# modeled as functions of the bottom left-coordinate
+# to each occupied space
+struct rock
+    points::Vector{Function}
+    highestPoint::Function
+
+    function rock(points::Vector{Function}, highestPoint::Function)
+        return new(points, highestPoint)
+    end
+end
+
+# builds the functions and the helper struct out of coordinate tuples,
+# with this definition, moving the pieces is easy.
+function makePiece(points::Vector{<:Tuple{<:Integer,<:Integer}})
+    pointFunctions = Vector{Function}()
+    highestPoint = 0
+
+    for (a, b) in points
+        func = (x, y) -> (x + a, y + b)
+        push!(pointFunctions, func)
+        highestPoint = max(highestPoint, b)
+    end
+
+    highestPointFunc = y -> y + highestPoint
+
+    return rock(pointFunctions, highestPointFunc)
+end
 
 
-function simulateRocks()
+# We make an assumption on a maximum grid size and either simulate as much rocks as needed
+# or we find a loop and skip ahead as far as possible, knowing the height-offset
+# after repeating this loop.
+function simulateRocks(NrOfRounds::Integer)
+    PiecePoints = [
+        [(0, 0), (1, 0), (2, 0), (3, 0)], # The - Piece
+        [(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)], # The + Piece
+        [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)], # The L Piece
+        [(0, 0), (0, 1), (0, 2), (0, 3)], # The | Piece
+        [(0, 0), (0, 1), (1, 0), (1, 1)] # The [] Piece
+    ]
 
-    line = first(eachline("./Day17/input.txt"))
-    streamLength = length(line)
+    # building all the pieces
+    Pieces = Vector{rock}()
+    for piece in PiecePoints
+        push!(Pieces, makePiece(piece))
+    end
 
-    highPoint = 4
-    currJet = 1
-    hasStopped = false
-    currY = 4
-    cave = falses(7,14000) #2022*5
-    
-    
-    for i in 0:2021
-        #println("Trying Piece ", mod(i,5), " with Bounds= ", Bounds)
-        Bounds = getPieceCoordinates(mod(i,5),highPoint)
-        #println("Trying Piece ", mod(i,5), " with Bounds= ", Bounds)
-        #println(Bounds)
-        while !hasStopped
+    Jet = first(eachline("./Day17/input.txt"))
+    JetPosition = 1
+    PieceNumber = 1
+    # the starting position
+    Position = (3, 4)
+    # the cave height is an assumption when we expect the loop.
+    # we could build this as a vector of vectors and update dynamically
+    # but a bitmatrix is significantly faster but cannot be rescaled
+    cave = falses(7, 10000)
 
-            movement = line[mod(currJet - 1,streamLength) + 1]
-            if movement == '<'
-                Bounds = moveHorizontal(Bounds,-1,cave)
+    # for loop detection, we keep a list of seen states
+    # with their height and number of placed pieces.
+    # each state consists of the current Position in the jetStream, the piece placed
+    # and the depth of each of the seven columns.
+    # Note that there are edge cases where this detects a cycle when there is none but 
+    # luckily this does not happen.
+    States = Dict{Vector{Integer},Tuple{<:Integer,<:Integer}}()
+
+    currState = ()
+    noCycleFound = true
+    highestPointFound = 1
+    NumberOfRocksPlaced = 0
+
+    while NumberOfRocksPlaced != NrOfRounds && noCycleFound
+
+        # this loops until the rock is sedentary
+        while true
+            movement = Jet[JetPosition]
+
+            # we move in the jet stream once
+            if movement == '>'
+                Position = move(Pieces[PieceNumber], Position, (1, 0), cave)
             else
-                Bounds = moveHorizontal(Bounds,1,cave)
+                Position = move(Pieces[PieceNumber], Position, (-1, 0), cave)
             end
-            currJet += 1
 
-            #printCave(cave,Bounds)
+            JetPosition = mod(JetPosition, length(Jet)) + 1
 
-            result = moveVertical(Bounds,-1,cave)
-            #println(result, " is this the same as ", Bounds)
-            if result == [[-1]]
-                hasStopped = true
-                highPoint = max(highestPoint(mod(i,5),currY) + 4,highPoint)
-                #println(highPoint)
-                for p in Bounds
-                    cave[p[1],p[2]] = true
+            # and then downwards once
+            newPosition = move(Pieces[PieceNumber], Position, (0, -1), cave)
+            if newPosition != Position
+
+                # if we can move downwards, do so and continue
+                Position = newPosition
+            else
+
+                # otherwise, we become sedentary and therefore fix our position and place the piece
+                placePiece(Pieces[PieceNumber], Position, cave)
+                NumberOfRocksPlaced += 1
+
+                # we also update the starting position for the next piece and our current height
+                highestPointFound = max(Pieces[PieceNumber].highestPoint(Position[2]), highestPointFound)
+                Position = (3, highestPointFound + 4)
+
+                # and add our current state to the seen states
+                currState = recordState(cave, highestPointFound, PieceNumber, JetPosition)
+
+                # but if we find a cycle, we can end here and just calculate the final height.
+                if haskey(States, currState)
+                    noCycleFound = false
+                else
+
+                    # otherwise, we add our current state and continue with the next piece
+                    States[currState] = (highestPointFound, NumberOfRocksPlaced)
                 end
-                #println(highestPoint(mod(i,5),currY))
-            else
-                currY += -1
-            end
 
-            #println(" ")
-            #printCave(cave,Bounds)
+                break
+            end
         end
-        currY = highPoint
-        hasStopped = false
+        PieceNumber = mod(PieceNumber, 5) + 1
     end
 
-    println(highPoint - 4)
-    #printCave(cave)
+    # if we found a cycle, we can repeat it as often as possible
+    # and add offsets at the beginning and end of the cycles to get our final height.
+    if !noCycleFound
+        loopEnd = NumberOfRocksPlaced
+        (_, loopStart) = States[currState]
+
+        CycleLength = loopEnd - loopStart
+        CycleHeightDifference = highestPointFound - findHeightAtTime(States, loopStart)
+
+        # Presumably our cycle does not start with the first piece,
+        # then we need to offset our cycle of cycles until the piece when it does start to repeat.
+        # finally, we might not repeat until we hit exactly our quota so we need to place some remaining Pieces.
+        (cycleRepetitions, NrOfRemainingPieces) = divrem(NrOfRounds - loopStart, CycleLength)
+
+        # the height we get for those remainingPieces can also be calculated from information we already have.
+        # Since the Number of remaining pieces is lower than the cycle length, the height we get
+        # by placing them can already be found in our list of states.
+        remainingHeight = findHeightAtTime(States, loopStart + NrOfRemainingPieces) - findHeightAtTime(States, loopStart)
+
+        # finally our final height is the offset at the beginning plus the height we get from repeating plus the offset at the end.
+        highestPointFound = findHeightAtTime(States, loopStart) + CycleHeightDifference * cycleRepetitions + remainingHeight
+    end
+
+    println(highestPointFound)
 end
 
-function simulateRocks2()
-
-    
-    line = first(eachline("./Day17/input.txt"))
-    streamLength = length(line)
-
-    highPoint = 4
-    currJet = 1
-    hasStopped = false
-    currY = 4
-
-    states = Set()
-    stateNumber = Dict()
-    cave = falses(7,50000) #2022*5
-    
-    foundDuplicate = false
-    remaining = 0
-    height = Int128(0)
-
-    heightbeforeEnd = 0
-
-
-    i = 0
-    while !foundDuplicate
-        #println("Trying Piece ", mod(i,5), " with Bounds= ", Bounds)
-        Bounds = getPieceCoordinates(mod(i,5),highPoint)
-        #println("Trying Piece ", mod(i,5), " with Bounds= ", Bounds)
-        #println(Bounds)
-        while !hasStopped
-
-            movement = line[mod(currJet - 1,streamLength) + 1]
-            if movement == '<'
-                Bounds = moveHorizontal(Bounds,-1,cave)
-            else
-                Bounds = moveHorizontal(Bounds,1,cave)
-            end
-            currJet += 1
-
-            #printCave(cave,Bounds)
-
-            result = moveVertical(Bounds,-1,cave)
-            #println(result, " is this the same as ", Bounds)
-            if result == [[-1]]
-                hasStopped = true
-                highPoint = max(highestPoint(mod(i,5),currY) + 4,highPoint)
-                #println(highPoint)
-                for p in Bounds
-                    cave[p[1],p[2]] = true
-                end
-                #println(highestPoint(mod(i,5),currY))
-            else
-                currY += -1
-            end
-
-            #println(" ")
-            #printCave(cave,Bounds)
+# a helper function
+# this just returns the recorded height at the recorded time in our list of states.
+function findHeightAtTime(States::Dict{Vector{Integer},Tuple{<:Integer,<:Integer}}, NrOfPiecesPlaced::Integer)
+    for (a, b) in values(States)
+        if b == NrOfPiecesPlaced
+            return a
         end
-        currY = highPoint
-        hasStopped = false
-        i += 1
-
-        depths = Vector{Integer}()
-        for j in 1:7
-            k = 0
-            while k != highPoint && !cave[j,highPoint - k]
-                k += 1
-            end
-            push!(depths,k)
-        end
-
-        if (depths,mod(i,5),mod(currJet -1,streamLength) + 1) ∈ states
-            foundDuplicate = true
-            occurence = stateNumber[(depths,mod(i,5),mod(currJet -1,streamLength) + 1)]
-            Heightdiff = highPoint - occurence[2]
-            NrDiff = i - occurence[1]
-
-            height += occurence[2] + 4
-
-            middle = div(1000000000000 - occurence[1],NrDiff)
-            height += Heightdiff * middle
-
-            remaining = 1000000000000 - occurence[1] - middle * NrDiff
-            heightbeforeEnd = highPoint
-        else
-            push!(states, (depths,mod(i,5),mod(currJet -1,streamLength) + 1))
-            push!(stateNumber, (depths,mod(i,5),mod(currJet -1,streamLength) + 1) => (i,highPoint))
-        end
-    end
-
-
-
-    for m in i:i+remaining-1
-        Bounds = getPieceCoordinates(mod(i,5),highPoint)
-        #println("Trying Piece ", mod(i,5), " with Bounds= ", Bounds)
-        #println(Bounds)
-        while !hasStopped
-
-            movement = line[mod(currJet - 1,streamLength) + 1]
-            if movement == '<'
-                Bounds = moveHorizontal(Bounds,-1,cave)
-            else
-                Bounds = moveHorizontal(Bounds,1,cave)
-            end
-            currJet += 1
-
-            #printCave(cave,Bounds)
-
-            result = moveVertical(Bounds,-1,cave)
-            #println(result, " is this the same as ", Bounds)
-            if result == [[-1]]
-                hasStopped = true
-                highPoint = max(highestPoint(mod(i,5),currY) + 4,highPoint)
-                #println(highPoint)
-                for p in Bounds
-                    cave[p[1],p[2]] = true
-                end
-                #println(highestPoint(mod(i,5),currY))
-            else
-                currY += -1
-            end
-
-            #println(" ")
-            #printCave(cave,Bounds)
-        end
-        currY = highPoint
-        hasStopped = false
-        i += 1
-    end
-
-    height += highPoint - heightbeforeEnd
-
-    println(remaining)
-    println(height - 8) #-8 for some reason
-    #println(highPoint - 4)
-    #printCave(cave)
-end
-
-function printCave(cave::BitMatrix, Bounds::Vector)
-    for i in axes(cave,2)
-        for j in axes(cave,1)
-            if cave[j,lastindex(cave,2)- (i-1)]
-                print("█")
-            elseif [j,lastindex(cave,2)- (i-1)] ∈ Bounds 
-                print("@")
-            else
-                print(" ")
-            end
-        end
-        println(" ")
     end
 end
 
-function getPieceCoordinates(PieceNumber::Integer,y::Integer)
-    if PieceNumber == 0
-        return Vector([[3,y],[4,y],[5,y],[6,y]])
-    elseif PieceNumber == 1
-        return Vector([[4,y],[3,y+1],[4,y+1],[5,y+1],[4,y+2]])
-    elseif PieceNumber == 2
-        return Vector([[3,y],[4,y],[5,y],[5,y+1],[5,y+2]])
-    elseif PieceNumber == 3
-        return Vector([[3,y],[3,y+1],[3,y+2],[3,y+3]])
-    elseif PieceNumber == 4
-        return Vector([[3,y],[4,y],[3,y+1],[4,y+1]])
+# this builds a state-tuple out of the current relevant information 
+function recordState(cave::BitMatrix, highestPointFound::Integer, PieceNumber::Integer, JetPosition::Integer)
+    depths = Vector{Integer}()
+    push!(depths, PieceNumber)
+    push!(depths, JetPosition)
+
+    for i in 1:7
+        j = highestPointFound
+        while true
+            if !checkbounds(Bool, cave, i, j) || cave[i, j]
+                push!(depths, highestPointFound - j)
+                break
+            else
+                j += -1
+            end
+        end
+    end
+
+    return depths
+end
+
+# after a piece has settled, we can fix it on the cave grid with this helper function
+function placePiece(piece::rock, position::Tuple{<:Integer,<:Integer}, cave::BitMatrix)
+    (x, y) = position
+
+    for f in piece.points
+        (a, b) = f(x, y)
+        cave[a, b] = true
+    end
+end
+
+# with this, we can move pieces or stand still, if we would collide with something.
+function move(piece::rock, position::Tuple{<:Integer,<:Integer}, offset::Tuple{<:Integer,<:Integer}, cave::BitMatrix)
+    canMove = true
+    (x, y) = position
+    (Δx, Δy) = offset
+
+    for f in piece.points
+        (a, b) = f(x + Δx, y + Δy)
+        if !checkbounds(Bool, cave, a, b) || cave[a, b]
+            canMove = false
+            break
+        end
+    end
+
+    if canMove
+        return (x + Δx, y + Δy)
     else
-        return Vector([[-1,-1]])
-    end
-end
-
-function highestPoint(PieceNumber::Integer,y::Integer)
-    if PieceNumber == 0
-        return y
-    elseif PieceNumber == 1
-        return y+2
-    elseif PieceNumber == 2
-        return y+2
-    elseif PieceNumber == 3
-        return y+3
-    elseif PieceNumber == 4
-        return y+1
-    else
-        return -1
-    end
-end
-
-function moveHorizontal(Positions::Vector,offset::Integer, cave::BitMatrix)
-    valid = true
-    for p in Positions
-        if !(0 < p[1] + offset < 8 && cave[p[1] + offset, p[2]] == false)
-            valid = false 
-        end
-    end
-    if valid
-        for p in Positions
-            p[1] += offset
-        end
-    end
-    return Positions
-end
-
-function moveVertical(Positions::Vector,offset::Integer, cave::BitMatrix)
-    valid = true
-    for p in Positions
-        if !(0 < p[2] + offset && cave[p[1],p[2] + offset] == false)
-            valid = false
-        end
-    end
-    if valid
-        for p in Positions
-            p[2] += offset
-        end
-        return Positions
-    else
-        return [[-1]]
+        return (x, y)
     end
 end
