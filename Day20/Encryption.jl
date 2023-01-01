e483df44ddbb0da3c288df769a08ae590c4f7c88
Jonas Seiler <Me@jonasseiler.de>
2023-01-01 20:16:07 +0100

    Cleaned up, optimized and added Explanation for Day20

--- Day20/Encryption.jl
+++ Day20/Encryption.jl
@@ -1,134 +1,80 @@
-using DataStructures
+# Solution to Puzzle 1 is Part1()
+# Solution to Puzzle 2 is Part2()
 
-mutable struct node
-    value::Integer
-    originalIndex::Integer
-    next::node
-    last::node
-
-    function node(value::Integer,originalIndex::Integer)
-        return new(value,originalIndex)
-    end
-
-    function node(value::Integer,originalIndex::Integer,next::node,last::node)
-        return new(value,originalIndex,next,last)
-    end
-end
-
-mutable struct List
-    start::node
-    length::Integer
-
-    function List(start::node)
-        start.next = start
-        start.last = start
-        return new(start,1)
-    end
-
-end
-
-function append!(l::List,n::node)
-    last = l.start.last
-    last.next = n
-    l.start.last = n
-    n.last = last
-    n.next = l.start
-    l.length += 1
-end
-
-function getNode(l::List,Nr::Integer)
-    node = l.start
-    while node.originalIndex != Nr
-        node = node.next
-    end
-    return node
-end
-
-function getbyValue(l::List,v::Integer)
-    node = l.start
-    while node.value != v
-        node = node.next
-    end
-    return node
-end
-
-function mix(thisnode::node,value::Integer)
-    if value == 0
-        return
-    end
-
-    targetnode = thisnode
-    if value > 0
-        for i in value:-1:1
-            targetnode = targetnode.next
-        end
-    elseif value < 0
-        targetnode = node.last
-        for i in 1:value
-            targetnode = targetnode.last
-        end
-    end
-
-    temp = targetnode.next
-    targetnode.next = thisnode
-    temp.last = thisnode
-    
-    thisnode.last.next = thisnode.next
-    thisnode.next.last = thisnode.last
-
-    thisnode.last = targetnode
-    thisnode.next = temp
-    return
-end
-
-function findAfter(thisnode::node,HowMany::Integer)
-    if HowMany == 0
-        thisnode
-    end
-
-    targetnode = thisnode
-    if HowMany > 0
-        for i in HowMany:-1:1
-            targetnode = targetnode.next
-        end
-    elseif HowMany < 0
-        targetnode = thisnode.last
-        for i in 1:HowMany
-            targetnode = targetnode.last
-        end
-    end
-
-    return targetnode
-end
-
+# helper function to parse the input into a number Vector
 function parseInput()
-    l = List
+    numbers = Vector{Integer}()
 
-    i = 1
     for line in eachline("./Day20/input.txt")
-        currNode = node(parse(Int,line) * 811589153,i) #changed
-        if i == 1
-            l = List(currNode)
-        else
-            append!(l,currNode)
-        end
-        i += 1
+        push!(numbers, parse(Int, line))
     end
-    return l
+
+    indices = collect(1:length(numbers))
+
+    return numbers, indices
 end
 
-function mixList()
-    l = parseInput()
-    for j in 1:10
-        for i in 1:l.length
-            n = getNode(l,i)
-            mix(n,mod(n.value,l.length-1))
-        end
+# helper function to get the index of an element in a Vector
+# assuming it is the only element of that value
+function getIndex(list::Vector{<:Integer}, value::Integer)
+    return findfirst(isequal(value), list)
+end
+
+# The main function, we only shuffle the vector of indices by the corresponding values in the number vector. 
+# By using modulus operations on the index, we don't need special
+# behaviour for negative indices and can save time on large values.
+function mix(indices::Vector{<:Integer}, numbers::Vector{<:Integer})
+    for i in 1:length(indices)
+        j = getIndex(indices, i)
+        deleteat!(indices, j)
+
+        # since Julia is 1-indexed, our modulo operation needs special offsets -_-
+        newPosition = mod(j + numbers[i] - 1, length(indices)) + 1
+        insert!(indices, newPosition, i)
+    end
+end
+
+# simple Solution for part1, we parse the input, shuffle the list of indices and then
+# finally translate the relevant indices to numbers and get our decryption key
+function Part1()
+    numbers, indices = parseInput()
+
+    mix(indices, numbers)
+
+    PositionOfZero = getIndex(indices, getIndex(numbers, 0))
+
+    decryptionSum = 0
+    for i in [1000, 2000, 3000]
+        # since Julia is 1-indexed, our modulo operation needs special offsets -_-
+        index = mod(PositionOfZero + i - 1, length(numbers)) + 1
+        value = numbers[indices[index]]
+        decryptionSum += value
     end
 
-    n = getbyValue(l,0)
-    first = findAfter(n,mod(1000,l.length)).value
-    second = findAfter(n,mod(2000,l.length)).value
-    third = findAfter(n,mod(3000,l.length)).value
-    println(first ,",", second, ",", third, " and sum: ", first+second+third)
+    println(decryptionSum)
+end
+
+# same thing as in part1, we parse our input and enlarge the numbers, then shuffle
+# the list 10 times and get our relevant indices again, finally we translate
+# those to numbers and add them up.
+function Part2()
+    numbers, indices = parseInput()
+
+    numbers .*= 811589153
+
+    for i in 1:10
+        mix(indices, numbers)
+    end
+
+    PositionOfZero = getIndex(indices, getIndex(numbers, 0))
+
+    decryptionSum = 0
+    for i in [1000, 2000, 3000]
+        # since Julia is 1-indexed, our modulo operation needs special offsets -_-
+        index = mod(PositionOfZero + i - 1, length(numbers)) + 1
+        value = numbers[indices[index]]
+        decryptionSum += value
+    end
+
+    println(decryptionSum)
 end
