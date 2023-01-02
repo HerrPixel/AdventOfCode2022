eb93f6d79febd1b6dc00ebd0c75744cc8bb757e7
Jonas Seiler <Me@jonasseiler.de>
2023-01-02 12:26:22 +0100

    Cleaned up, optimized and added Explanation for Day24

--- Day24/Blizzard.jl
+++ Day24/Blizzard.jl
@@ -1,292 +1,154 @@
 using DataStructures
 
+# Solution to Puzzle 1 is Part1()
+# Solution to Puzzle 2 is Part2()
+
+# helper struct modelling a Blizzard
+mutable struct Blizzard
+    x::Integer
+    y::Integer
+    Orientation::Integer
+
+    # Orientation 1 is right, 2 is down, 3 is left and 4 is up
+    function Blizzard(x::Integer, y::Integer, Orientation::Integer)
+        return new(x, y, Orientation)
+    end
+end
+
+# Parsing each Blizzard into its respective struct while also 
+# find the width and height of the grid
 function parseInput()
-    file = "./Day24/input.txt"
-    grid = zeros(1, 1)
-
-    height = countlines(file)
     width = 0
-    start = (0, 0)
-    goal = (0, 0)
+    height = 0
+    Blizzards = Vector{Blizzard}()
 
-    DownwardBlizzards = Vector{Tuple{<:Integer,<:Integer}}()
-    UpwardBlizzards = Vector{Tuple{<:Integer,<:Integer}}()
-    RightwardBlizzards = Vector{Tuple{<:Integer,<:Integer}}()
-    LeftwardBlizzards = Vector{Tuple{<:Integer,<:Integer}}()
+    i = 0
+    for line in eachline("./Day24/input.txt")
+        i += 1
+        width = length(line)
 
-    j = 1
-    for line in eachline(file)
-        if width == 0
-            width = length(line)
-            grid = zeros(Int, width, height)
-        end
-
-        i = 1
+        j = 0
         for c in line
-            if c == '#'
-                grid[i, j] = -1
-            elseif c == '.'
-                if j == 1
-                    start = (i, j)
-                elseif j == height
-                    goal = (i, j)
-                end
-            elseif c == '>'
-                grid[i, j] += 1
-                push!(RightwardBlizzards, (i, j))
-            elseif c == '<'
-                grid[i, j] += 1
-                push!(LeftwardBlizzards, (i, j))
-            elseif c == '^'
-                grid[i, j] += 1
-                push!(UpwardBlizzards, (i, j))
+            j += 1
+            if c == '>'
+                push!(Blizzards, Blizzard(j, i, 1))
             elseif c == 'v'
-                grid[i, j] += 1
-                push!(DownwardBlizzards, (i, j))
-            end
-            i += 1
-        end
-        j += 1
-    end
-
-    return grid, start, goal, UpwardBlizzards, DownwardBlizzards, RightwardBlizzards, LeftwardBlizzards
-end
-
-function drawGrid(grid)
-    for j in axes(grid, 2)
-        for i in axes(grid, 1)
-            if grid[i, j] == -1
-                print("#")
-            elseif grid[i, j] == 0
-                print(".")
-            else
-                print(grid[i, j])
+                push!(Blizzards, Blizzard(j, i, 2))
+            elseif c == '<'
+                push!(Blizzards, Blizzard(j, i, 3))
+            elseif c == '^'
+                push!(Blizzards, Blizzard(j, i, 4))
             end
         end
-        println("")
     end
+
+    height = i
+
+    return Blizzards, width, height
 end
 
-function BlizzardBFS()
-    grid, start, goal, UpwardBlizzards, DownwardBlizzards, RightwardBlizzards, LeftwardBlizzards = parseInput()
+# creating an empty base grid with only the walls.
+# each step then copies this grid and populates it with Blizzards
+function createBaseGrid(width::Integer, height::Integer)
+    BaseGrid = falses(width, height)
 
-    #println(start)
-    Positions = Set{Tuple{<:Integer,<:Integer}}()
-    push!(Positions, start)
+    # the walls
+    for i in 1:width
+        BaseGrid[i, 1] = true
+        BaseGrid[i, height] = true
+    end
 
+    for j in 1:height
+        BaseGrid[1, j] = true
+        BaseGrid[width, j] = true
+    end
+
+    # the two open spots for the beginning and end
+    BaseGrid[2, 1] = false
+    BaseGrid[width-1, height] = false
+
+    return BaseGrid
+end
+
+# we create our grid and blizzards and then just use BFS to get the fastest way
+function Part1()
+    Blizzards, width, height = parseInput()
+
+    BaseGrid = createBaseGrid(width, height)
+
+    time = BFS(BaseGrid, Blizzards, (2, 1), (width - 1, height))
+
+    println(time)
+end
+
+# Like in Part1, we use BFS to get the fastest Paths, this time only three times.
+function Part2()
+    Blizzards, width, height = parseInput()
+
+    BaseGrid = createBaseGrid(width, height)
+
+    time1 = BFS(BaseGrid, Blizzards, (2, 1), (width - 1, height))
+    time2 = BFS(BaseGrid, Blizzards, (width - 1, height), (2, 1))
+    time3 = BFS(BaseGrid, Blizzards, (2, 1), (width - 1, height))
+
+    println(time1 + time2 + time3)
+end
+
+# each step we first update the Blizzards Positions, 
+# then populate the grid at that time step and finally
+# calculate the next possible Posiitons out of the current ones and alle directions to go.
+function BFS(BaseGrid::BitMatrix, Blizzards::Vector{Blizzard}, start::Tuple{<:Integer,<:Integer}, goal::Tuple{<:Integer,<:Integer})
+    possiblePositions = [start]
     time = 0
-    finished = false
 
-    while !finished
+    # all directions we can go from a space
+    movements = [(0, 0), (1, 0), (0, -1), (-1, 0), (0, 1)]
 
+    while true
         time += 1
-        #println("In Loop: ", time)
 
-        grid, DownwardBlizzards = moveBlizzardsDown(grid, DownwardBlizzards)
-        grid, LeftwardBlizzards = moveBlizzardsLeft(grid, LeftwardBlizzards)
-        grid, RightwardBlizzards = moveBlizzardsRight(grid, RightwardBlizzards)
-        grid, UpwardBlizzards = moveBlizzardsUp(grid, UpwardBlizzards)
+        # we always copy the baseGrid with the walls 
+        # and then populate that with the Blizzards Positions
+        grid = copy(BaseGrid)
+        moveBlizzards(Blizzards, size(grid, 1), size(grid, 2))
 
-        #drawGrid(grid)
+        for b in Blizzards
+            grid[b.x, b.y] = true
+        end
 
-        newPositions = Set{Tuple{<:Integer,<:Integer}}()
-        for (x, y) in Positions
-            #println("Considering ", (x, y))
-            if grid[x, y] == 0
-                push!(newPositions, (x, y))
-            end
-            if grid[x+1, y] == 0
-                push!(newPositions, (x + 1, y))
-            end
-            if grid[x-1, y] == 0
-                push!(newPositions, (x - 1, y))
-            end
-            if grid[x, y+1] == 0
-                if (x, y + 1) == goal
-                    finished = true
-                    break
-                else
-                    push!(newPositions, (x, y + 1))
+        NewPositions = Set{Tuple{<:Integer,<:Integer}}()
+
+        # calculating each new Position out of the old ones
+        for (x, y) in possiblePositions
+            for (Δx, Δy) in movements
+                if !checkbounds(Bool, grid, x + Δx, y + Δy)
+                    continue
                 end
-            end
-            if y - 1 > 0 && grid[x, y-1] == 0
-                if (x, y - 1) != start
-                    push!(newPositions, (x, y - 1))
+
+                if (x + Δx, y + Δy) == goal
+                    return time
+                end
+
+                if grid[x+Δx, y+Δy] == false
+                    push!(NewPositions, (x + Δx, y + Δy))
                 end
             end
         end
 
-        Positions = newPositions
-
+        possiblePositions = NewPositions
     end
-
-    println("First goal: ", time)
-
-    finished = false
-
-    Positions = Set{Tuple{<:Integer,<:Integer}}()
-    push!(Positions, goal)
-
-    while !finished
-
-        time += 1
-        #println("In Loop: ", time)
-
-        grid, DownwardBlizzards = moveBlizzardsDown(grid, DownwardBlizzards)
-        grid, LeftwardBlizzards = moveBlizzardsLeft(grid, LeftwardBlizzards)
-        grid, RightwardBlizzards = moveBlizzardsRight(grid, RightwardBlizzards)
-        grid, UpwardBlizzards = moveBlizzardsUp(grid, UpwardBlizzards)
-
-        #drawGrid(grid)
-
-        newPositions = Set{Tuple{<:Integer,<:Integer}}()
-        for (x, y) in Positions
-            #println("Considering ", (x, y))
-            if grid[x, y] == 0
-                push!(newPositions, (x, y))
-            end
-            if grid[x+1, y] == 0
-                push!(newPositions, (x + 1, y))
-            end
-            if grid[x-1, y] == 0
-                push!(newPositions, (x - 1, y))
-            end
-            if y + 1 < size(grid, 2) && grid[x, y+1] == 0
-                push!(newPositions, (x, y + 1))
-            end
-            if y - 1 > 0 && grid[x, y-1] == 0
-                if (x, y - 1) == start
-                    finished = true
-                    break
-                end
-                if (x, y - 1) != start
-                    push!(newPositions, (x, y - 1))
-                end
-            end
-        end
-
-        Positions = newPositions
-
-    end
-
-    println("second goal: ", time)
-
-    finished = false
-
-    Positions = Set{Tuple{<:Integer,<:Integer}}()
-    push!(Positions, start)
-
-    while !finished
-
-        time += 1
-        #println("In Loop: ", time)
-
-        grid, DownwardBlizzards = moveBlizzardsDown(grid, DownwardBlizzards)
-        grid, LeftwardBlizzards = moveBlizzardsLeft(grid, LeftwardBlizzards)
-        grid, RightwardBlizzards = moveBlizzardsRight(grid, RightwardBlizzards)
-        grid, UpwardBlizzards = moveBlizzardsUp(grid, UpwardBlizzards)
-
-        #drawGrid(grid)
-
-        newPositions = Set{Tuple{<:Integer,<:Integer}}()
-        for (x, y) in Positions
-            #println("Considering ", (x, y))
-            if grid[x, y] == 0
-                push!(newPositions, (x, y))
-            end
-            if grid[x+1, y] == 0
-                push!(newPositions, (x + 1, y))
-            end
-            if grid[x-1, y] == 0
-                push!(newPositions, (x - 1, y))
-            end
-            if grid[x, y+1] == 0
-                if (x, y + 1) == goal
-                    finished = true
-                    break
-                else
-                    push!(newPositions, (x, y + 1))
-                end
-            end
-            if y - 1 > 0 && grid[x, y-1] == 0
-                if (x, y - 1) != start
-                    push!(newPositions, (x, y - 1))
-                end
-            end
-        end
-
-        Positions = newPositions
-
-    end
-
-
-    println("final time: ", time)
 end
 
-function moveBlizzardsDown(grid, DownwardBlizzards)
-    newBlizzards = Vector{Tuple{<:Integer,<:Integer}}()
-    for (x, y) in DownwardBlizzards
-        grid[x, y] += -1
-        if grid[x, y+1] == -1
-            push!(newBlizzards, (x, 2))
-            grid[x, 2] += 1
-        else
-            push!(newBlizzards, (x, y + 1))
-            grid[x, y+1] += 1
-        end
+# each blizzard updates its position according to its orientation and wraps around, 
+# if it hits a wall
+# curiously, the blizzards wrapping around can be modeled with a modulo operation
+# with careful offsets
+function moveBlizzards(Blizzards::Vector{Blizzard}, width::Integer, height::Integer)
+    directionDeltas = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    for b in Blizzards
+        (Δx, Δy) = directionDeltas[b.Orientation]
+        b.x = mod(b.x + Δx - 2, width - 2) + 2
+        b.y = mod(b.y + Δy - 2, height - 2) + 2
     end
-
-    return grid, newBlizzards
-    #drawGrid(grid)
-end
-
-function moveBlizzardsUp(grid, UpwardBlizzards)
-    newBlizzards = Vector{Tuple{<:Integer,<:Integer}}()
-    for (x, y) in UpwardBlizzards
-        grid[x, y] += -1
-        if grid[x, y-1] == -1
-            push!(newBlizzards, (x, size(grid, 2) - 1))
-            grid[x, size(grid, 2)-1] += 1
-        else
-            push!(newBlizzards, (x, y - 1))
-            grid[x, y-1] += 1
-        end
-    end
-
-    return grid, newBlizzards
-    #drawGrid(grid)
-end
-
-function moveBlizzardsRight(grid, RightwardBlizzards)
-    newBlizzards = Vector{Tuple{<:Integer,<:Integer}}()
-    for (x, y) in RightwardBlizzards
-        grid[x, y] += -1
-        if grid[x+1, y] == -1
-            push!(newBlizzards, (2, y))
-            grid[2, y] += 1
-        else
-            push!(newBlizzards, (x + 1, y))
-            grid[x+1, y] += 1
-        end
-    end
-
-    return grid, newBlizzards
-    #drawGrid(grid)
-end
-
-function moveBlizzardsLeft(grid, LeftwardBlizzards)
-    newBlizzards = Vector{Tuple{<:Integer,<:Integer}}()
-    for (x, y) in LeftwardBlizzards
-        grid[x, y] += -1
-        if grid[x-1, y] == -1
-            push!(newBlizzards, (size(grid, 1) - 1, y))
-            grid[size(grid, 1)-1, y] += 1
-        else
-            push!(newBlizzards, (x - 1, y))
-            grid[x-1, y] += 1
-        end
-    end
-
-    return grid, newBlizzards
-    #drawGrid(grid)
 end
